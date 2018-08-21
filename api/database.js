f5e00870bde094a739f3907e9455c231e957367b
CO <co.petstore123@gmail.com>
2018-08-21 10:29:26 +0800

    5 use dotenv to manage the secrets

--- api/database.js
+++ api/database.js
@@ -1,9 +1,11 @@
 const mysql=require('mysql');
+require('dotenv').config();
+
 const connection=mysql.createPool({
-    host     : 'localhost',
-    port     : '3306',
-    user     : 'root',
-    password : 'abcd1234',
-    database : 'petstore123'
+    host     : process.env.DB_HOST,
+    port     : process.env.DB_PORT,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE
 });
 module.exports=connection;
